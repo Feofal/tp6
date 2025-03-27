574a3a1211f848f00775d28c63a3f4fbf4066880
Feofal <forielfusierg@educ.college-montreal.qc.ca>
2025-03-27 13:23:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
+"""
+    Nom: Gabriel Foriel Fusier
+    Groupe: 401
+    Description: Un jeu de roche, papier, ciseaux
+"""
 import arcade
-from enum import Enum
-import game_state
+from game_state import GameState
+from attack_animation import *
 import random
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 800
-SCREEN_TITLE = "Mahna Mahna Do doo be-do-do"
+SCREEN_TITLE = "RPC"
 class RPCGAME(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.LICORICE)
         self.player_point = 0
         self.ai_point = 0
-        self.ai_face = arcade.Sprite("assets/compy.png", 1.5, SCREEN_WIDTH/2 + 250, SCREEN_HEIGHT/2 + 20)
-        self.player_face = arcade.Sprite("assets/faceBeard.png", 0.3, SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/2 + 20)
-        self.rock = arcade.Sprite("assets/srock.png", 0.5, SCREEN_WIDTH/2 - 350, SCREEN_HEIGHT/2 - 80)
-        self.paper = arcade.Sprite("assets/spaper.png", 0.5, SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/2 - 80)
-        self.cissors = arcade.Sprite("assets/scissors.png", 0.5, SCREEN_WIDTH/2 - 150, SCREEN_HEIGHT/2 - 80)
-        self.ai_rock = arcade.Sprite("assets/srock.png", 0.5, SCREEN_WIDTH / 2 + 250, SCREEN_HEIGHT / 2 - 80)
-        self.ai_paper = arcade.Sprite("assets/spaper.png", 0.5, SCREEN_WIDTH / 2 + 250, SCREEN_HEIGHT / 2 - 80)
-        self.ai_cissors = arcade.Sprite("assets/scissors.png", 0.5, SCREEN_WIDTH / 2 + 250, SCREEN_HEIGHT / 2 - 80)
+        self.ai_face = arcade.Sprite("assets/compy.png", 1.5,
+                                     SCREEN_WIDTH/2 + 250, SCREEN_HEIGHT/2 + 20)
+        self.player_face = arcade.Sprite("assets/faceBeard.png", 0.3,
+                                         SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/2 + 20)
+        self.rock = AttackAnimation(AttackType.ROCK)
+        self.rock.center_x = SCREEN_WIDTH/2 - 350
+        self.rock.center_y = SCREEN_HEIGHT/2 - 80
+        self.paper = AttackAnimation(AttackType.PAPER)
+        self.paper.center_x = SCREEN_WIDTH/2 - 250
+        self.paper.center_y = SCREEN_HEIGHT/2 - 80
+        self.cissors = AttackAnimation(AttackType.CISSORS)
+        self.cissors.center_x = SCREEN_WIDTH / 2 - 150
+        self.cissors.center_y = SCREEN_HEIGHT / 2 - 80
+        self.ai_rock = AttackAnimation(AttackType.ROCK)
+        self.ai_rock.center_x = SCREEN_WIDTH / 2 + 250
+        self.ai_rock.center_y = SCREEN_HEIGHT / 2 - 80
+        self.ai_paper = AttackAnimation(AttackType.PAPER)
+        self.ai_paper.center_x = SCREEN_WIDTH / 2 + 250
+        self.ai_paper.center_y = SCREEN_HEIGHT / 2 - 80
+        self.ai_cissors = AttackAnimation(AttackType.CISSORS)
+        self.ai_cissors.center_x = SCREEN_WIDTH / 2 + 250
+        self.ai_cissors.center_y = SCREEN_HEIGHT / 2 - 80
         self.players_list = arcade.SpriteList()
         self.rock_list = arcade.SpriteList()
         self.paper_list = arcade.SpriteList()
@@ -28,9 +47,10 @@ class RPCGAME(arcade.Window):
         self.rock_list.append(self.rock)
         self.paper_list.append(self.paper)
         self.cissors_list.append(self.cissors)
-        self.state = game_state.GameState.NOT_STARTED
+        self.state = GameState.NOT_STARTED
         self.player_attack = 3
         self.ai_attack = None
+        self.subtitle = "Appuyez sur ESPACE pour commencer"
         self.attack_list = {
             0: "rock",
             1: "paper",
@@ -39,74 +59,104 @@ class RPCGAME(arcade.Window):
         }
     def on_draw(self):
         self.clear()
+        point_color = [arcade.color.LIBERTY, arcade.color.RAZZLE_DAZZLE_ROSE, arcade.color.RED_DEVIL, arcade.color.LEMON_YELLOW]
         game_title = arcade.Text("Roche - Papier - Ciseau",
                                  SCREEN_WIDTH/2, SCREEN_HEIGHT - 100,
                                  arcade.color.ROYAL_YELLOW, 50, bold= True,
                                  anchor_x= "center")
-        game_subtitle = arcade.Text("Appuyer sur une image pour faire une attaque!",
+        game_subtitle = arcade.Text(self.subtitle,
                                     SCREEN_WIDTH/2, SCREEN_HEIGHT - 150,
                                     arcade.color.LIGHT_BLUE, 25,
                                     bold= True, anchor_x= "center")
         player_text = arcade.Text(f"Vous avez {self.player_point} points",
-                                  SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/10, arcade.color.LIBERTY,
+                                  SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/10, point_color[self.player_point],
                                   20, bold= True, anchor_x= "center")
         ai_text = arcade.Text(f"L'ia a {self.ai_point} points",
                               SCREEN_WIDTH/2 + 250, SCREEN_HEIGHT/10,
-                              arcade.color.LIBERTY, 20,
+                              point_color[self.ai_point], 20,
                               bold= True, anchor_x= "center")
-        self.victory_text = arcade.Text("VICTOIRE", SCREEN_WIDTH/2, SCREEN_HEIGHT - 200, arcade.color.GOLD, 50, bold= True, anchor_x= "center")
-        self.loose_text = arcade.Text("DÉFAITE", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 200, arcade.color.RED_DEVIL, 50,
-                                        bold=True, anchor_x="center")
-        self.retry_text = arcade.Text("Appuyer sur ÉSPACE pour recommencer", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 250, arcade.color.LIGHT_BLUE, 20,
-                                        bold=True, anchor_x="center")
+        self.victory_text = arcade.Text("VICTOIRE",
+                                        SCREEN_WIDTH/2, SCREEN_HEIGHT - 200,
+                                        arcade.color.GOLD, 50,
+                                        bold= True, anchor_x= "center")
+        self.loose_text = arcade.Text("DÉFAITE",
+                                      SCREEN_WIDTH / 2, SCREEN_HEIGHT - 200,
+                                      arcade.color.RED_DEVIL, 50,
+                                      bold=True, anchor_x="center")
+        self.retry_text = arcade.Text("Appuyer sur ESPACE pour recommencer",
+                                      SCREEN_WIDTH / 2, SCREEN_HEIGHT - 250,
+                                      arcade.color.LIGHT_BLUE, 20,
+                                      bold=True, anchor_x="center")
         game_title.draw()
-        if self.state == game_state.GameState.GAME_OVER:
+        if self.state == GameState.GAME_OVER:
             if self.player_point == 3:
                 self.victory_text.draw()
             else:
                 self.loose_text.draw()
             self.retry_text.draw()
+            self.rock.animating = False
+            self.paper.animating = False
+            self.cissors.animating = False
+            self.ai_rock.animating = False
+            self.ai_paper.animating = False
+            self.ai_cissors.animating = False
         else:
+            self.rock.animating = True
+            self.paper.animating = True
+            self.cissors.animating = True
+            self.ai_rock.animating = True
+            self.ai_paper.animating = True
+            self.ai_cissors.animating = True
             game_subtitle.draw()
         player_text.draw()
         ai_text.draw()
 
         for i in range(0,3):
-            arcade.draw_circle_outline(SCREEN_WIDTH/2 - 150 - i*100, SCREEN_HEIGHT/2 - 85, 60, arcade.color.ROYAL_AZURE, tilt_angle= 45, num_segments= 4)
-        arcade.draw_circle_outline(SCREEN_WIDTH / 2 + 250, SCREEN_HEIGHT / 2 - 85, 55,
-                                   arcade.color.ROYAL_AZURE, tilt_angle=45, num_segments=4)
+            arcade.draw_circle_outline(SCREEN_WIDTH/2 - 150 - i*100,
+                                       SCREEN_HEIGHT/2 - 85, 60,
+                                       arcade.color.ROYAL_AZURE,
+                                       tilt_angle= 45, num_segments= 4)
+        arcade.draw_circle_outline(SCREEN_WIDTH / 2 + 250, SCREEN_HEIGHT / 2 - 85,
+                                   55, arcade.color.ROYAL_AZURE,
+                                   tilt_angle=45, num_segments=4)
         self.players_list.draw()
         self.rock_list.draw()
         self.paper_list.draw()
         self.cissors_list.draw()
 
     def on_key_press(self, symbol: int, modifiers: int):
-        if self.state == game_state.GameState.NOT_STARTED or self.state == game_state.GameState.GAME_OVER and arcade.key.SPACE:
-            self.state = game_state.GameState.ROUND_ACTIVE
+        if self.state == GameState.NOT_STARTED or self.state == GameState.GAME_OVER and arcade.key.SPACE:
+            self.subtitle = "Appuyer sur une image pour faire une attaque!"
+            self.state = GameState.ROUND_ACTIVE
             self.player_point = 0
             self.ai_point = 0
+        elif self.state == GameState.ROUND_DONE and arcade.key.SPACE:
+            self.subtitle = "Appuyer sur une image pour faire une attaque!"
+            self.state = GameState.ROUND_ACTIVE
+            self.player_attack = 3
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         if (self.rock.collides_with_point((x,y))
             or self.paper.collides_with_point((x,y))
                 or self.cissors.collides_with_point((x,y))):
-            if self.state == game_state.GameState.ROUND_DONE:
-                self.state = game_state.GameState.ROUND_ACTIVE
             if self.rock.collides_with_point((x,y)):
                 self.player_attack = 0
             elif self.paper.collides_with_point((x,y)):
                 self.player_attack = 1
-                print(self.player_attack)
             elif self.cissors.collides_with_point((x,y)):
                 self.player_attack = 2
-                print(self.player_attack)
-
 
     def on_update(self, delta_time: float):
+        self.rock.on_update()
+        self.paper.on_update()
+        self.cissors.on_update()
+        self.ai_rock.on_update()
+        self.ai_paper.on_update()
+        self.ai_cissors.on_update()
         if self.player_point == 3 or self.ai_point == 3:
-            self.state = game_state.GameState.GAME_OVER
+            self.state = GameState.GAME_OVER
         self.ai_attack = random.randint(0,2)
-        if self.state == game_state.GameState.ROUND_ACTIVE and self.attack_list[self.player_attack] != "choice not made":
+        if self.state == GameState.ROUND_ACTIVE and self.attack_list[self.player_attack] != "choice not made":
             self.both_attack = [self.attack_list[self.player_attack], self.attack_list[self.ai_attack]]
             match self.both_attack:
                 case ["rock", "cissors"]:
@@ -137,7 +187,8 @@ class RPCGAME(arcade.Window):
                     self.paper_list.append(self.ai_paper)
                 case "cissors":
                     self.cissors_list.append(self.ai_cissors)
-            self.state = game_state.GameState.ROUND_DONE
+            self.subtitle = "Appuyez sur ESPACE pour continuer"
+            self.state = GameState.ROUND_DONE
 def main():
     rpc = RPCGAME(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
