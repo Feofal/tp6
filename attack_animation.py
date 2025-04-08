ae27c646fc630adc209b51d67d961c0940ddcfc3
Feofal <forielfusierg@educ.college-montreal.qc.ca>
2025-04-08 10:05:31 -0400

    Update attack_animation.py

--- attack_animation.py
+++ attack_animation.py
@@ -1,18 +1,23 @@
+from enum import Enum
+import arcade
+
+
 class AttackType(Enum):
     ROCK = 0,
     PAPER = 1,
     CISSORS = 2
 
+
 class AttackAnimation(arcade.Sprite):
     ATTACK_SCALE = 0.5
     ANIMATION_SPEED = 5
+
     def __init__(self, attack_type):
         super().__init__()
         self.animation_update_time = 1 / AttackAnimation.ANIMATION_SPEED
         self.current_texture = 0
         self.time_since_last_swap = 0
         self.attack_type = AttackType
-        self.animating = True
         match attack_type:
             case AttackType.ROCK:
                 self.textures = [
@@ -29,14 +34,12 @@ class AttackAnimation(arcade.Sprite):
                     arcade.load_texture("assets/scissors.png"),
                     arcade.load_texture("assets/scissors-close.png")
                 ]
-
-
         self.scale = AttackAnimation.ATTACK_SCALE
         self.set_texture(self.current_texture)
 
     def on_update(self, delta_time: float = 1/60):
         self.time_since_last_swap += delta_time
-        if self.time_since_last_swap > self.animation_update_time and self.animating == True:
+        if self.time_since_last_swap > self.animation_update_time:
             self.current_texture += 1
             if self.current_texture < len(self.textures):
                 self.set_texture(self.current_texture)
